03f55b1593a2ad2367e5e576a62b0a9dd86b5bad
HugoLA1 <hugo.leboeuf@viacesi.fr>
2017-06-19 16:02:53 +0200

    Change name of Rockford class to Player class

--- BoulderDash/model/src/main/java/model/Player.java
+++ BoulderDash/model/src/main/java/model/Player.java
@@ -5,23 +5,23 @@ import java.sql.SQLException;
 import java.util.List;
 import java.awt.event.KeyEvent;
 
-public class Rockford extends Character implements IModel {
-	Point rockford;
+public class Player extends Character implements IModel {
+	Point playerPosition;
 
 	public void Movement(KeyEvent e) {
-		Point p = rockford.getLocation();
+		Point p = playerPosition.getLocation();
 		switch (e.getKeyCode()){
 			case KeyEvent.VK_RIGHT:
-				rockford.setLocation(p.x+1, p.y);
+				playerPosition.setLocation(p.x+1, p.y);
 				break;
 			case KeyEvent.VK_LEFT:
-				rockford.setLocation(p.x-1, p.y);
+				playerPosition.setLocation(p.x-1, p.y);
 				break;
 			case KeyEvent.VK_UP:
-				rockford.setLocation(p.x, p.y-1);
+				playerPosition.setLocation(p.x, p.y-1);
 				break;
 			case KeyEvent.VK_DOWN:
-				rockford.setLocation(p.x, p.y+1);
+				playerPosition.setLocation(p.x, p.y+1);
 				break;}}
 
 	@Override
