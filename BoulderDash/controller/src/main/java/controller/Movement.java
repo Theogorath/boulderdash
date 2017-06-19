fd5f65befe0ea869021fb08b47b4c8e5969bd92e
HugoLA1 <hugo.leboeuf@viacesi.fr>
2017-06-19 15:24:36 +0200

    KeyListener for the player Rockford

---
+++ BoulderDash/controller/src/main/java/controller/Movement.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public interface Movement extends KeyListener{
+
+
+	public void Movement(KeyEvent e);
+		
+
+
+}
--- BoulderDash/model/src/main/java/model/Rockford.java
+++ BoulderDash/model/src/main/java/model/Rockford.java
@@ -1,5 +1,46 @@
 package model;
 
-public class Rockford extends Character{
-	
+import java.awt.Point;
+import java.sql.SQLException;
+import java.util.List;
+import java.awt.event.KeyEvent;
+
+public class Rockford extends Character implements IModel {
+	Point rockford;
+
+	public void Movement(KeyEvent e) {
+		Point p = rockford.getLocation();
+		switch (e.getKeyCode()){
+			case KeyEvent.VK_RIGHT:
+				rockford.setLocation(p.x+1, p.y);
+				break;
+			case KeyEvent.VK_LEFT:
+				rockford.setLocation(p.x-1, p.y);
+				break;
+			case KeyEvent.VK_UP:
+				rockford.setLocation(p.x, p.y-1);
+				break;
+			case KeyEvent.VK_DOWN:
+				rockford.setLocation(p.x, p.y+1);
+				break;}}
+
+	@Override
+	public Example getExampleById(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Example getExampleByName(String name) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Example> getAllExamples() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 }
