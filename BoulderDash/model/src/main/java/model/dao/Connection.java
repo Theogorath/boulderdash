880aad222a92d4e58288a650fc3e04085047fef6
EvaPierrini <eva.pierrini@viacesi.fr>
2017-06-21 00:28:27 +0200

    n

---
+++ BoulderDash/model/src/main/java/model/dao/Connection.java
@@ -0,0 +1,28 @@
+package model.dao;
+
+import java.sql.DriverManager;
+
+import com.mysql.cj.api.jdbc.Statement;
+
+public class Connection {
+
+	public void connection (String[] args){	//connection to the database
+		
+			String url = "jdbc:mysql://localhost/test?useSSL=false&serverTimezone=UTC"; //database url
+			String user = "root"; //user name
+			String password = ""; //user password
+			
+	try{
+		
+		Connection connection = DriverManager.getConnection(this.url, this.user, this.password);
+		Statement statement = (Statement) ((java.sql.Connection) connection).createStatement();
+		ResultSet result = statement.executeQuery("CALL getLevel("+ id +")");  // TO DO recover the level id
+	}
+	
+	catch (Exception e){
+		e.printStackTrace();
+	}
+		
+	}
+	
+}
