15e62587790e98809209f399ec0a86a40d2a2f1b
EvaPierrini <eva.pierrini@viacesi.fr>
2017-06-21 00:29:13 +0200

    dirt gravity 1

--- BoulderDash/model/src/main/java/model/Dirt.java
+++ BoulderDash/model/src/main/java/model/Dirt.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Dirt extends Static {
+	
+	char dirtPosition;
+	boolean dirtIsVisible;
+	
 
+	//when player position = dirt position, dirt remove
+	public void dirtRemove(){
+		
+		if (playerPosition = dirtPosition){ //TO DO: recover playerPosition
+			dirtIsVisible = false;
+		}
+		
+		else{
+			dirtIsVisible = true;
+		}
+		
+	}
+	
+	//TO DO: add the dirt sprite
+	
 }
--- BoulderDash/model/src/main/java/model/GravityElement.java
+++ BoulderDash/model/src/main/java/model/GravityElement.java
@@ -1,5 +1,29 @@
 package model;
 
 public class GravityElement extends Mobile{
+	
+	//When dirtIsVisible = false under Gravity element
+	
+	if (xDirt = xGravityElement){
+		
+		if (yDirt = yGravityElement-1){
+			yGravityElement = yGravityElement-1;
+		}
+		
+		else {
+			//do nothing
+		}
+		
+	}
+	
+	else if (){
+		//case when the gravityElement is under an other gravityElement without element next to
+	}
+	
+	else{
+		//do nothing
+	}
+		
+		
 
 }
