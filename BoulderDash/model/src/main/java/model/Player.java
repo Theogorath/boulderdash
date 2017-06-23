754d4645e14e2c09c7599b2cd2392597763f78e1
HugoLA1 <hugo.leboeuf@viacesi.fr>
2017-06-23 11:35:33 +0200

    Player can now see the map and move

--- BoulderDash/model/src/main/java/model/Player.java
+++ BoulderDash/model/src/main/java/model/Player.java
@@ -25,11 +25,11 @@ public class Player extends Character implements IController {
 	 *            This event occurs when a key press is followed by a key release       
 	 */
 	Map mapLevel = new Map();
-	public void Movement(KeyEvent e) {
+	public void Movement(KeyEvent e) throws InterruptedException {
 		Point p = playerPosition.getLocation();
 		switch (e.getKeyCode()){
 		case KeyEvent.VK_RIGHT:
-			if(mapLevel.getValue(Integer.parseInt(p.x+1), p.y) != 2){
+			if(mapLevel.getValue(p.x+1, p.y).getId() != 2){
 				playerPosition.setLocation(p.x+1, p.y);
 				new ShowSprite(2,48,64); 
 				Thread.sleep(100);
@@ -38,7 +38,7 @@ public class Player extends Character implements IController {
 
 			break;
 		case KeyEvent.VK_LEFT:
-			if(mapLevel[p.x-1][p.y] != 2){
+			if(mapLevel.getValue(p.x-1, p.y).getId() != 2){
 				playerPosition.setLocation(p.x-1, p.y);
 				new ShowSprite(2,48,32); 
 				Thread.sleep(100);
@@ -46,15 +46,15 @@ public class Player extends Character implements IController {
 			}
 			break;
 		case KeyEvent.VK_UP:
-			if(mapLevel[p.x][p.y-1] != 2 && mapLevel[p.x][p.y-1] != 3){
-				playerPosition.setLocation(p.x, p.y-1);
+			if(mapLevel.getValue(p.x, p.y+1).getId() != 2 && mapLevel.getValue(p.x, p.y+1).getId() != 3){
+				playerPosition.setLocation(p.x, p.y+1);
 				new ShowSprite(2,32,48); 
 				Thread.sleep(100);
 				new ShowSprite(2,16,16);
 			}
 			break;
 		case KeyEvent.VK_DOWN:
-			if(mapLevel[p.x][p.y+1] != 2 && mapLevel[p.x][p.y+1] != 3){
+			if(mapLevel.getValue(p.x, p.y-1).getId() != 2 && mapLevel.getValue(p.x, p.y-1).getId() != 3){
 				playerPosition.setLocation(p.x, p.y-1);
 				new ShowSprite(2,32,80); 
 				Thread.sleep(100);
